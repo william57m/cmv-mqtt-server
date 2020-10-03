4812e7eaf51c972fbb8a3351b4f7ee8d04bb0893
William Mura <william.mura@autodesk.com>
2020-10-03 10:46:55 -0400

    Add timer to autoturn off the fan

--- cmv.py
+++ cmv.py
@@ -1,7 +1,8 @@
 import codes
+import threading
 
-from enum import Enum
 from rpi_rf import RFDevice
+
 
 class CMV:
 
@@ -14,14 +15,27 @@ class CMV:
     self.rfdevice.enable_tx()
     self.rfdevice.tx_repeat = 10
 
+    # Fan timer
+    self.timer_fan_restroom = threading.Timer(15, target=self.set_state_fan_restroom, args=[False])
+    self.timer_fan_restroom.daemon = True
+
   def reset(self, commit=True):
     self.fan_restroom = False
 
     if commit:
       self.commit()
 
+  def set_state_fan_restroom(self, state):
+    self.fan_restroom = state
+
   def toggle_fan_restroom(self):
     self.fan_restroom = not self.fan_restroom
+
+    if self.fan_restroom:
+      self.timer_fan_restroom.start()
+    else:
+      self.timer_fan_restroom.cancel()
+
     self.commit(codes.CODE_TOGGLE_RELAY)
 
   def get_state(self, key=None):
